e6e69c2d600072f6b6cbe393e33f110c89c188d4
Do Trung Hoai <trunghoai.do@samsung.com>
2023-04-28 14:58:34 +0700

    Add carthage

---
+++ Cores/HomeModule/HomeModule/HomeModule.h
@@ -0,0 +1,18 @@
+//
+//  HomeModule.h
+//  HomeModule
+//
+//  Created by iOS_hoai_new on 28/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HomeModule.
+FOUNDATION_EXPORT double HomeModuleVersionNumber;
+
+//! Project version string for HomeModule.
+FOUNDATION_EXPORT const unsigned char HomeModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HomeModule/PublicHeader.h>
+
+
---
+++ Cores/HomeModule/HomeModule/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  HomeModule
+//
+//  Created by iOS_hoai_new on 28/04/2023.
+//
+
+import RxSwift
+
+class HomeViewModel {
+    
+}
