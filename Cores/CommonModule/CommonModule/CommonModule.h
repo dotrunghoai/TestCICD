8e4feea71b12ca697ea99ff27057ee44d358f790
Do Trung Hoai <trunghoai.do@samsung.com>
2023-05-04 15:55:10 +0700

    Edit yml file

---
+++ Cores/CommonModule/CommonModule/CommonModule.h
@@ -0,0 +1,18 @@
+//
+//  CommonModule.h
+//  CommonModule
+//
+//  Created by iOS_hoai_new on 04/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonModule.
+FOUNDATION_EXPORT double CommonModuleVersionNumber;
+
+//! Project version string for CommonModule.
+FOUNDATION_EXPORT const unsigned char CommonModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonModule/PublicHeader.h>
+
+
