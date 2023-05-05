c7cd10d19624982f394fd5654961ed84c07a65df
Do Trung Hoai <trunghoai.do@samsung.com>
2023-05-05 08:12:36 +0700

    Edit yml file

--- TestCICD/ViewViewModel.swift
+++ TestCICD/ViewViewModel.swift
@@ -6,11 +6,13 @@
 //
 
 import RxSwift
+import SocketIO
 
 class ViewViewModel {
     let publisher1 = PublishSubject<String>()
     
     func emitsValue() {
         publisher1.onNext("123")
+        let socket = SocketManager(socketURL: URL(string: "1")!)
     }
 }
